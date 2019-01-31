0888b2dfa02e5f1c63ae8c0cd296bd2cb4baa036
tyler-niemi <42221010+tyler-niemi@users.noreply.github.com>
2019-01-31 14:12:18 -0800

    Add files via upload

---
+++ js/openproject.js
@@ -0,0 +1,16 @@
+function openTrips() {
+    window.open("trips.html");
+}
+
+function openShop() {
+    window.open("donut_shop.html");
+}
+
+function openBlackJack() {
+    window.open("blackjack/index.html");
+}
+
+function openCheckers() {
+    window.open("checkers/checkers.html");
+}
+
